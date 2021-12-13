76af6c316ef7af60e029f8778e427142ddee2a40
lurt04 <82723622+lurt04@users.noreply.github.com>
2021-12-13 14:45:33 +0100

    Add files via upload

---
+++ Source/MarioForzaTurismo/MarioForzaTurismo.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MarioForzaTurismo.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MarioForzaTurismo, "MarioForzaTurismo" );
---
+++ Source/MarioForzaTurismo/MarioForzaTurismoGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MarioForzaTurismoGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MARIOFORZATURISMO_API AMarioForzaTurismoGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
